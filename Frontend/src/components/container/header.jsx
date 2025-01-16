50b4118f23832db5efd2e417f23e7749d28adc03
ChallaSreeja <challasreeja2004@gmail.com>
2025-01-16 17:44:23 +0530

    header and row containers for table are made

---
+++ Frontend/src/components/container/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import DarkLabel from './darkLabel'; // Assuming DarkLabel is in this path
+
+const StockTableHeader = () => {
+  return (
+    <div className="grid grid-cols-6 gap-4 mb-6">
+      <DarkLabel>Stock Name</DarkLabel>
+      <DarkLabel>Ticker</DarkLabel>
+      <DarkLabel>Quantity</DarkLabel>
+      <DarkLabel>Date of Buy</DarkLabel>
+      <DarkLabel>Buy Price</DarkLabel>
+      <DarkLabel>Action</DarkLabel>
+    </div>
+  );
+};
+
+export default StockTableHeader;

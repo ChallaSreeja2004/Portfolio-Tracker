8c75d4b55250be0ea451e1515e71efa7e3a58f80
ChallaSreeja <challasreeja2004@gmail.com>
2025-01-16 23:11:43 +0530

    table components and containers made

---
+++ Frontend/src/components/tableComponents/lightLabel.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const LightLabel = ({ children }) => {
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md text-center">
+      {children}
+    </div>
+  );
+};
+export default LightLabel;
